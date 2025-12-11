a67062609c7b93926978b1c8887158202c9d7a7e
.newmind <git.newmind@newmind.pw>
2025-12-11 20:05:12 +0100

    move runix-code to submodule

--- apps/runix-code/lib/GeneratedTools.hs
+++
@@ -1,8 +0,0 @@
-module GeneratedTools where
-
-import UniversalLLM.Core.Tools
-import Polysemy
-import Data.Text (Text)
-import Autodocodec (HasCodec(..))
-
--- Generated tools will be appended below
